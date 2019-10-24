7c4779f0135be027283227246adc71eb5b098140
Jaz Walker <playallday723@gmail.com>
2019-10-24 15:27:16 -0500

    1st commit oct 24 3:27 PM

---
+++ strings and stuff.py
@@ -0,0 +1,29 @@
+# Strings
+
+
+# Concatenation
+# 2 strings or more and put them together
+
+
+firstName = "Joe"
+lastName = "Mama"
+
+print(firstName + " " + lastName)
+
+name = firstName + " " + lastName
+lastFirst = lastName + ", " + firstName
+
+print(lastFirst)
+
+# Repetition
+
+print("Hip "*4 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, "*3 + "your boat")
+    print("Gently down the stream")
+    print("Merrily, "*4)
+    print("Life is but a dream")
+
+rowYourBoat()
+
